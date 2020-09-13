046c48c280d37d46b9ad37e053e5932e66208728
Samuel Thompson <thomps9012@gmail.com>
2020-09-13 13:10:28 -0400

    added in an index.js

--- exercise-log/src/index.js
+++ exercise-log/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import '!style!css!bootstrap/dist/css/bootstrap.css';
+import '!style!css!bootstrap/dist/css/bootstrap-theme.css';
+import '!style!css!./styles/main.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Root from './components/Root';
+
+render(<Root />, document.getElementById('app'));
