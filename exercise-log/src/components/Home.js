0a1c990bc84c78d161944a5776d51e6bff5ff29b
Samuel Thompson <thomps9012@gmail.com>
2020-09-13 09:49:49 -0400

    added in list component

--- exercise-log/src/components/Home.js
+++ exercise-log/src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
             <Col xs={12}>
                 {/* add in an image and some custom styling here */}
                 <h1>It's time to get yoked and fit!</h1>
-                <p>Have you ever wanted to track your runs, weight training sessions, and general fitness workouts in one convienent place? Insert name here, helps you keep track of your exercises so you can focus on putting in that work.</p>
+                <p>Have you ever wanted to track your runs, weight training sessions, and general fitness workouts in one convienent place? Health Hub, helps you keep track of your exercises so you can focus on putting in that work.</p>
                 <ButtonToolbar>
                     <LinkContainer to="/signup">
                         <Button bsStyle="primary">Sign Up</Button>
