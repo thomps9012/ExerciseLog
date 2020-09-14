4f473c969dbb25befee687abcad2f1f300a27269
Samuel Thompson <thomps9012@gmail.com>
2020-09-14 09:33:00 -0400

    installed react router dom

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -5,15 +5,15 @@ import Button from 'react-bootstrap/Button';
 const PasswordForget = () => (
     <div>
         <Form>
-        <h1>PasswordForget</h1>   
-        <Form.Group controlId="formBasicEmail">
+            <h1>PasswordForget</h1>
+            <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
                 <Button variant="primary" type="submit">
-                Reset Password
+                    Reset Password
   </Button>
             </Form.Group>
         </Form>
