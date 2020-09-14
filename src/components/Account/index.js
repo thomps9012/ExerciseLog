321f0b5267b3803adbf32bb73af0aa1e0cf00151
Samuel Thompson <thomps9012@gmail.com>
2020-09-14 09:32:11 -0400

    added in Account and password forget page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Account = () => (
     <div>
         <h1>Account</h1>
+        
+        <Form>
+        <h2>Reset Password</h2>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                <Button variant="primary" type="submit">
+                Reset Password
+  </Button>
+            </Form.Group>
+        </Form>
+        <Form>
+        <h2>Change Password</h2>    
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Confirm New Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Update Password
+  </Button>
+        </Form>
     </div>
 );
 
--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const PasswordForget = () => (
     <div>
-        <h1>PasswordForget</h1>
+        <Form>
+        <h1>PasswordForget</h1>   
+        <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                <Button variant="primary" type="submit">
+                Reset Password
+  </Button>
+            </Form.Group>
+        </Form>
     </div>
 );
 
